3012d1770db87ce527a836d527b7528105721c7b
VinceBorgyEspina <202180167@psu.palawan.edu.ph>
2023-11-07 08:30:25 +0800

    faker installed

---
+++ ph_db.php
@@ -0,0 +1,29 @@
+<?php
+class Database {
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = 'borgyvince321';
+    private $database = 'ph_locale';
+    private $conn;
+
+    public function __construct() {
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->database}";
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+
+    public function getConnection() {
+        echo 'Connected';
+        return $this->conn;
+    }
+    
+}
+
+
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vince/records_app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a80fbc73dfde0c7697e86a5616255e760bd0d18a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fzaninotto/faker' => array(
+            'pretty_version' => 'v1.5.0',
+            'version' => '1.5.0.0',
+            'reference' => 'd0190b156bcca848d401fb80f31f504f37141c8d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fzaninotto/faker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vince/records_app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a80fbc73dfde0c7697e86a5616255e760bd0d18a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
