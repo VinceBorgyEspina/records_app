5d36a30ae696a0071f0b6274859f5b045c0bc282
VinceBorgyEspina <202180167@psu.palawan.edu.ph>
2023-11-13 21:11:46 +0800

    Config added

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php 
+    define  ('ROOT_URL','http://localhost/recordsapp/');
+    define ('DB_HOST','localhost');
+    define ('DB_USER','root');
+    define ('DB_PASS','borgyvince321');
+    define ('DB_NAME','records_app');
